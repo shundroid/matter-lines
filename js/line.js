cf8466ab9e1698e351b964a6742d807fb13d966f
shundroid <shundroidk@gmail.com>
2017-03-11 13:56:19 +0900

    added line.js

---
+++ js/line.js
@@ -0,0 +1,52 @@
+export default class Line {
+  constructor(defaultPoints = [], lineWidth = 10) {
+    this.points = defaultPoints;
+    this.outsidePoints = [];
+    this.insidePoints = [];
+    this.lineWidth = lineWidth;
+
+    if (this.points.length > 0) {
+      this.caluclatePoints();
+    }
+  }
+  addPoint(point) {
+    this.points.push(point);
+  }
+  caluclatePoints() {
+    this.outsidePoints = [];
+    this.insidePoints = [];
+    this.points.forEach((point, index) => {
+      if (index === 0) {
+        const rad =
+          Math.atan2(this.points[index + 1].y - point.y, this.points[index + 1].x - point.x) - Math.PI / 2;
+        const sin = Math.sin(rad) * this.lineWidth;
+        const cos = Math.cos(rad) * this.lineWidth;
+        this.outsidePoints.push({ x: point.x + cos, y: point.y + sin });
+        this.insidePoints.push({ x: point.x - cos, y: point.y - sin });
+      } else if (index === this.points.length - 1) {
+        const rad =
+          Math.atan2(point.y - this.points[index - 1].y, point.x - this.points[index - 1].x) - Math.PI / 2;
+        const sin = Math.sin(rad) * this.lineWidth;
+        const cos = Math.cos(rad) * this.lineWidth;
+        this.outsidePoints.push({ x: point.x + cos, y: point.y + sin });
+        this.insidePoints.push({ x: point.x - cos, y: point.y - sin });
+      } else {
+        let rad1 = Math.atan2(this.points[index - 1].y - point.y, this.points[index - 1].x - point.x);
+        let rad2 = Math.atan2(this.points[index + 1].y - point.y, this.points[index + 1].x - point.x);
+        if (rad1 < 0) rad1 += Math.PI * 2;
+        if (rad2 < 0) rad2 += Math.PI * 2;
+        const rad = (rad2 - rad1) / 2;
+        const x = Math.cos(rad) * this.lineWidth / Math.sin(rad);
+        const y = this.lineWidth;
+        // 回転移動させる
+        const rx = x * Math.cos(rad1) - y * Math.sin(rad1);
+        const ry = x * Math.sin(rad1) + y * Math.cos(rad1);
+        this.outsidePoints.push({ x: point.x + rx, y: point.y + ry });
+        this.insidePoints.push({ x: point.x - rx, y: point.y - ry });
+      }
+    });
+  }
+  getVertices() {
+    return this.outsidePoints.concat(this.insidePoints.reverse());
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { Engine, Render, World, Bodies } from "matter-js";
+import Line from "./line";
 
 document.addEventListener("DOMContentLoaded", () => {
 /*  // create an engine
@@ -32,91 +33,36 @@ document.addEventListener("DOMContentLoaded", () => {
     boxB.vertices[0].y = 0;
   }, 100);*/
 
-  function toRadian(deg) {
-    return deg * Math.PI / 180;
-  }
-
-  function getLinearFunction(x1, y1, x2, y2) {
-    // A1x + B1y = E1
-    // A2x + B2y = E2
-    // x = (E1 * B2 - B1 * E2) / (A1 * B2 - B1 * A2)
-    // y = (E2 * A1 - E1 * A2) / (A1 * B2 - A2 * B1)
-
-    // x: a, y: b
-    // A1: x1, B1: 1, E1: y1
-    // A2: x2, B2: 1, E2: y2
-    const a = (y1 - y2) / (x1 - x2);
-    const b = (y2 * x1 - y1 * x2) / (x1 - x2);
-    return { a, b };
-  }
-
-  const lineWidth = 10;
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
 
   const points = [
     { x: 250, y: 70 },
     { x: 150, y: 100 },
-    { x: 100, y: 40 }
+    { x: 400, y: 100 }
   ];
-
-  const outsideLine = [];
-  const insideLine = [];
-
-  ctx.strokeStyle = "black";
-  ctx.beginPath();
-  points.forEach((vector, index) => {
-    if (index === 0) {
-      ctx.moveTo(vector.x, vector.y);
-      let rad = Math.atan2(points[index + 1].y - vector.y, points[index + 1].x - vector.x);
-      rad += toRadian(-90);
-      outsideLine.push({ x: vector.x + Math.cos(rad) * lineWidth, y: vector.y + Math.sin(rad) * lineWidth });
-      rad += toRadian(180);
-      insideLine.push({ x: vector.x + Math.cos(rad) * lineWidth, y: vector.y + Math.sin(rad) * lineWidth });
-    } else if (index === points.length - 1) {
-      let rad = Math.atan2(vector.y - points[index - 1].y, vector.x - points[index - 1].x);
-      rad += toRadian(-90);
-      outsideLine.push({ x: vector.x + Math.cos(rad) * lineWidth, y: vector.y + Math.sin(rad) * lineWidth });
-      rad += toRadian(180);
-      insideLine.push({ x: vector.x + Math.cos(rad) * lineWidth, y: vector.y + Math.sin(rad) * lineWidth });
-    } else {
-      let rad1 = Math.atan2(points[index - 1].y - vector.y,  points[index - 1].x - vector.x);
-      let rad2 = Math.atan2(points[index + 1].y - vector.y,  points[index + 1].x - vector.x);
-      if (rad1 < 0) rad1 += toRadian(360);
-      if (rad2 < 0) rad2 += toRadian(360);
-      let rad = (rad2 - rad1) / 2;
-      console.log(rad * 180 / Math.PI, rad1 * 180 / Math.PI, rad2 * 180 / Math.PI);
-      const y = lineWidth;
-      const x = Math.cos(rad) * lineWidth / Math.sin(rad);
-      // 回転移動させる
-      const rx = x * Math.cos(rad1) - y * Math.sin(rad1);
-      const ry = x * Math.sin(rad1) + y * Math.cos(rad1);
-      outsideLine.push({ x: vector.x + rx, y: vector.y + ry });
-      insideLine.push({ x: vector.x - rx, y: vector.y - ry });
-    }
-    ctx.lineTo(vector.x, vector.y);
-  });
-  ctx.stroke();
+  const line = new Line(points);
 
   ctx.strokeStyle = "red";
   ctx.beginPath();
-  outsideLine.forEach((vector, index) => {
+  line.points.forEach((point, index) => {
     if (index === 0) {
-      ctx.moveTo(vector.x, vector.y);
+      ctx.moveTo(point.x, point.y);
     } else {
-      ctx.lineTo(vector.x, vector.y);
+      ctx.lineTo(point.x, point.y);
     }
-  });
-  ctx.stroke();
+  })
+  ctx.stroke()
 
-  ctx.strokeStyle = "blue";
+  ctx.strokeStyle = "black";
   ctx.beginPath();
-  insideLine.forEach((vector, index) => {
+  line.getVertices().forEach((point, index) => {
     if (index === 0) {
-      ctx.moveTo(vector.x, vector.y);
+      ctx.moveTo(point.x, point.y);
     } else {
-      ctx.lineTo(vector.x, vector.y);
+      ctx.lineTo(point.x, point.y);
     }
-  });
-  ctx.stroke();
+  })
+  ctx.stroke()
+
 });
