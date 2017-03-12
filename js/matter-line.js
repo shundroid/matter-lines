f2ae62fa06da14a07b2dabb0a3ae6d68fddcb4b4
shundroid <shundroidk@gmail.com>
2017-03-12 13:03:36 +0900

    supporteed concave with poly-decomp

--- js/matter-line.js
+++ js/matter-line.js
@@ -1,5 +1,5 @@
 import Line from "./line";
-import { Bodies, World, Body } from "matter-js";
+import { Bodies, World, Body, Vertices } from "matter-js";
 import { EventEmitter } from "events";
 export default class MatterLine extends Line {
   constructor(world, defaultPoints = [], lineWidth = 10, miterLimit = 10) {
@@ -20,12 +20,16 @@ export default class MatterLine extends Line {
       }
       this.body = null;
     } else {
-      if (this.body === null) {
-        this.body = Body.create();
-        Body.setStatic(this.body, true);
-        World.add(this.world, this.body);
+      if (this.body !== null) {
+        World.remove(this.world, this.body);
       }
-      this.body.vertices = vertices;
+      this.body = Bodies.fromVertices(0, 0, vertices);
+      World.add(this.world, this.body);
+      Body.setPosition(this.body, {
+        x: this.body.position.x - this.body.bounds.min.x + 10,
+        y: this.body.position.y - this.body.bounds.min.y
+      });
+      Body.setStatic(this.body, true);
     }
   }
 }
